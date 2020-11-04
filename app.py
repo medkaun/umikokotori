a93cf59a3eda8f4ba8b513ade19dfa0b8a644747
MacBook Pro <bookbook@MacBooks-MacBook-Pro.local>
2020-11-04 21:41:21 +0200

    added templates

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("umikokotorien.html")
+
+@app.route("/umiko")
+def umikoen():
+    return render_template("umikoen.html")
+
+@app.route("/kotori")
+def kotorien():
+    return render_template("kotorien.html")
 
 if __name__ == "__main__":
     app.run(app, debug=True)
